b6c5464db7c236b0ec7db5118bf051304ebb60fd
Akpk1234 <akashsh888@gmail.com>
2024-10-17 11:36:39 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exception_Project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number: ");
+            try
+            {
+                string userInput = Console.ReadLine();
+                int number = Convert.ToInt32(userInput);
+                Console.WriteLine("You entered: " + number);
+            }   
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Error: Please enter a valid numeric value.");
+            }
+        }
+    }
+}
